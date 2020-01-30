9706de7f78421baf02ab699a08b8c2080009f58c
Felipe Balbi <felipe@balbi.sh>
2020-01-30 20:33:34 +0200

    Disable the Navigation until the Step is Valid
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,25 +1,26 @@
 <template>
-  <div>
-    <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/>
-    <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
-    <FormAddress v-if="currentStepNumber === 3" @update="processStep"/>
-    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep"/>
-
-    <div class="progress-bar">
-      <div :style="`width: ${progress}%;`"></div>
-    </div>
-
-    <!-- Actions -->
-    <div class="buttons">
-      <button
-        @click="goBack"
-        v-if="currentStepNumber > 1"
-        class="btn-outlined"
+<div>
+  <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/>
+  <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
+  <FormAddress v-if="currentStepNumber === 3" @update="processStep"/>
+  <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep"/>
+  
+  <div class="progress-bar">
+    <div :style="`width: ${progress}%;`"></div>
+  </div>
+  
+  <!-- Actions -->
+  <div class="buttons">
+    <button
+      @click="goBack"
+      v-if="currentStepNumber > 1"
+      class="btn-outlined"
       >Back
-      </button>
-      <button
-        @click="goNext"
-        class="btn"
+    </button>
+    <button
+      @click="goNext"
+      :disabled="!canGoNext"
+      class="btn"
       >Next</button>
     </div>
 
@@ -43,6 +44,7 @@ export default {
   data () {
     return {
       currentStepNumber: 1,
+      canGoNext: false,
       length: 4,
       form: {
         plan: null,
@@ -64,12 +66,14 @@ export default {
   methods: {
     processStep (stepData) {
       Object.assign(this.form, stepData)
+      this.canGoNext = true
     },
     goBack () {
       this.currentStepNumber--
     },
     goNext () {
       this.currentStepNumber++
+      this.canGoNext = false
     }
   }
 }
